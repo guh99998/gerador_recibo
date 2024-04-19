53b10a6bcf25f6574ff20ef2ebb8800013c1ff6e
Gustavo Lopes <guhrlopes999@gmail.com>
2024-04-19 20:05:55 -0300

    criado recibo.py

---
+++ recibo.py
@@ -0,0 +1,11 @@
+class Recibo:
+    def __init__(self, nome, valor, referencia, data, cidade, estado):
+        self._nome = nome
+        self._valor = valor
+        self._referencia = referencia
+        self._data = data
+        self._cidade = cidade
+        self._estado = estado
+
+    def criar_recibo():
+        pass
