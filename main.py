f9d6a6bd2d76cd5df3fee8df978bff124047a78b
Gustavo Lopes <guhrlopes999@gmail.com>
2024-04-19 15:04:01 -0300

    alterado main.py

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from docx.shared import Inches
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import RGBColor
+from modules.numero_extenso import number_to_long_number
+from modules.format_values import formatar_doc
 
-nome_recibo = "teste"#input('Digite o nome do recibo criado: ')#
-nome_cliente = "nome_cliente"#input
-valor_recibo = "valor_recibo"#input
-cpf_cliente = "cpf_cliente"#input e opcional
-referencia_recibo = "referencia recibo (colocar o texto que vai após 'referente a ...')"#input
-data_recibo = "data_recibo"#input
-local_recibo = "local_recibo"#input
-recebedor = "recebedor"#input
-doc_recebedor = "documento recebedor"#input
+nome_recibo = "Recibo arrendamento de pasto - Adriano"#input('Digite o nome do recibo criado: ')#
+nome_cliente = "Adriano Aparecido de Lima"#input str
+valor_recibo = 350.00#input float
+cpf_cliente = "05410912632"#input e opcional str
+referencia_recibo = "pagamento do arrendamento do pasto para colocar gado no Jardim dos Lagos, na cidade Águas de Lindóia - SP. (04 cabeças), referente ao mês de Fevereiro de 2024"#input str
+data_recibo = "19/04/2024"#input str
+cidade_recibo = "Monte Sião"#input str
+estado_recibo = "MG"
+recebedor = "recebedor"#input str
+doc_recebedor = "documento recebedor"#input str
 
 document = Document()
 
@@ -24,9 +27,11 @@ run_valor = valor.add_run(f'R${valor_recibo}')
 run_valor.font.size = Pt(16)
 valor.alignment = WD_ALIGN_PARAGRAPH.RIGHT
 
-corpo = document.add_paragraph()
-run_corpo = corpo.add_run('''
+corpo = document.add_paragraph('Recebi de "')
+corpo.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+run_corpo = corpo.add_run({nome_cliente.upper()})
+run_corpo.bold = True
+corpo.add_run(f'", inscrito no CPF n° {formatar_doc(cpf_cliente)}, a importância de R${valor_recibo:.2f} ({number_to_long_number(valor_recibo)}), referente ao {referencia_recibo}.')
 
-''')
 
 document.save(f'{nome_recibo}.docx')
