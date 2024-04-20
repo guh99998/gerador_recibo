c2279dfe454e11fb019ab4786139e1426da23aef
Gustavo Lopes <guhrlopes999@gmail.com>
2024-04-20 10:43:55 -0300

    alterado pessoa.py

--- models/pessoa.py
+++ models/pessoa.py
@@ -5,11 +5,16 @@ class Pessoa:
 
     def nome_upper(self):
         ''''
-        Formata uma string recebida, deixando a string inteira com letras maiúsculas
+        Formata uma string recebida, deixando a string inteira com letras maiúsculas.
         '''
         return self._nome.upper()
 
     def tipo_pessoa(self):
+        '''
+        Verifica se a string recebida tem o tamanho para ser um CPF ou se não é considerada um CNPJ.
+
+        :return: String contendo o formato do documento.
+        '''
         if len(self._documento) == 11:
             return 'CPF'
         else:
