721df9752a30555eb5fcec57547b46a58598d10b
Gustavo Lopes <guhrlopes999@gmail.com>
2024-04-19 21:20:10 -0300

    alterado classe pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
     def __init__(self, nome, documento):
-        self.nome = nome
-        self.documento = documento
+        self._nome = nome
+        self._documento = documento
+
+    def nome_upper(self):
+        return self._nome.upper()
