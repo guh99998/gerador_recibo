76d398bf34e5a7ebc0148cbebb6b456676db7374
Gustavo Lopes <guhrlopes999@gmail.com>
2024-04-20 10:44:35 -0300

    alterado format_data.py

--- modules/format_data.py
+++ modules/format_data.py
@@ -3,6 +3,12 @@ import datetime, locale
 locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
 def formatar_data(data_str):
+    '''
+    Formata uma data 'DD/MM/YYYY' para 'DD de MM de YYYY', ou seja, deixa uma data por extenso.
+
+    :param data_str: String representando a data.
+    :return: String formatada da data.
+    '''
     data_obj = datetime.datetime.strptime(data_str, "%d/%m/%Y")
     data_formatada = data_obj.strftime("%d de %B de %Y")
     return data_formatada
