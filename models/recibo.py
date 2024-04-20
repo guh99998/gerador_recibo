d4c56998da2ba25af9a1d4a64a47af2939dfe0b3
Gustavo Lopes <guhrlopes999@gmail.com>
2024-04-19 22:29:30 -0300

    alterado recibo.py

--- models/recibo.py
+++ models/recibo.py
@@ -1,9 +1,9 @@
 from docx import Document
 from docx.shared import Pt
-from docx.enum.text import WD_ALIGN_PARAGRAPH
 from modules.format_documento import formatar_doc
 from modules.format_data import formatar_data
 from modules.format_numero_extenso import formatar_numero_extenso
+from constants import *
 
 class Recibo:
     def __init__(self, pagador, recebedor, nome, valor, referencia, data, cidade, estado):
@@ -18,20 +18,20 @@ class Recibo:
         self._estado = estado
 
     def add_titulo(self):
-        titulo = self.document.add_heading("RECIBO", level=0)
-        titulo.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        titulo = self.document.add_heading(TITULO, level=0)
+        titulo.alignment = ALINHAMENTO_CENTRAL
 
     def add_valor(self):
         valor = self.document.add_paragraph()
         run_valor = valor.add_run(f"R${self._valor:.2f}")
-        run_valor.font.size = Pt(20)
-        valor.alignment = WD_ALIGN_PARAGRAPH.RIGHT
-        valor.paragraph_format.space_before = Pt(24)
-        valor.paragraph_format.space_after = Pt(72)
+        run_valor.font.size = VALOR_FONT_SIZE
+        valor.alignment = ALINHAMENTO_DIREITA
+        valor.paragraph_format.space_before = ESPACAMENTO_24
+        valor.paragraph_format.space_after = ESPACAMENTO_72
 
     def add_corpo(self):
         corpo = self.document.add_paragraph('Recebi de "')
-        corpo.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        corpo.alignment = ALINHAMENTO_JUSTIFICADO
         run_corpo = corpo.add_run({self._pagador._nome.upper()})
         run_corpo.bold = True
         corpo.add_run(
@@ -40,29 +40,29 @@ class Recibo:
 
     def add_data_assinatura(self):
         data = self.document.add_paragraph()
-        data.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        data.alignment = ALINHAMENTO_CENTRAL
         data_run = data.add_run(
             f"{self._cidade} - {self._estado.upper()}, {formatar_data(self._data)}."
         )
-        data.paragraph_format.space_after = Pt(72)
-        data.paragraph_format.space_before = Pt(50)
+        data.paragraph_format.space_after = ESPACAMENTO_72
+        data.paragraph_format.space_before = ESPACAMENTO_50
 
         assinatura = self.document.add_paragraph()
         assinatura.add_run("\n\n")
         assinatura.add_run(
             "__________________________________________________________________________"
         )
-        assinatura.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        assinatura.alignment = ALINHAMENTO_CENTRAL
 
     def add_recebedor(self):
         nome_recebedor = self.document.add_paragraph()
-        nome_recebedor.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        nome_recebedor.alignment = ALINHAMENTO_CENTRAL
         nome_recebedor_run = nome_recebedor.add_run(f"{self._recebedor.nome_upper()}")
         documento_recebedor = self.document.add_paragraph()
         documento_recebedor_run = documento_recebedor.add_run(
             f"{self._recebedor.tipo_pessoa()}: {formatar_doc(self._recebedor._documento)}"
         )
-        documento_recebedor.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        documento_recebedor.alignment = ALINHAMENTO_CENTRAL
 
     def salvar_recibo(self):
         self.document.save(f"{self._nome}.docx")
