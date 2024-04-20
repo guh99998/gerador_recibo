572d1c1954d48d5755538b72dac6f6a2164b64aa
Gustavo Lopes <guhrlopes999@gmail.com>
2024-04-20 10:44:25 -0300

    alterado format_documento.py

--- modules/format_documento.py
+++ modules/format_documento.py
@@ -1,4 +1,11 @@
 def formatar_doc(documento):
+    '''
+    Formata um documento no formato '12345678912' para '123.456.789-12' ou
+    '1234567891234' para '12.345.678/912-34'.
+
+    :param documento: String representando o documento.
+    :return: String formatada do documento.
+    '''
     if len(documento) == 11:
         documento = documento.zfill(11)
         cpf = '{}.{}.{}-{}'.format(documento[:3], documento[3:6], documento[6:9], documento[9:])
